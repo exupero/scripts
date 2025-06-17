f88c3d23ea8f68ae184cdc3b5f62ca3cdcc73ef4
Eric Shull <eric@exupero.org>
2025-06-17 17:13:09 -0400

    Check in

--- preload.clj
+++ preload.clj
@@ -1,7 +1,9 @@
 (require '[babashka.deps :as deps])
 (deps/add-deps '{:deps {hashp/hashp {:mvn/version "0.2.2"}
-                        mvxcvi/puget {:mvn/version "1.3.4"}}})
-(require 'hashp.core)
+                        mvxcvi/puget {:mvn/version "1.3.4"}
+                        swiss-arrows/swiss-arrows {:mvn/version "1.0.0"}}})
+(require 'hashp.core
+         '[swiss.arrows :refer [-<> -<>>]])
 
 (defmacro defp [nm & body]
   `(do (def ~nm ~@body)
