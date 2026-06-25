1212b2dbd116b794f06123decd6560b39960605c
Eric Shull <eric@exupero.org>
2026-06-25 16:39:29 -0400

    Check in

--- lib/xforms.clj
+++ lib/xforms.clj
@@ -5,17 +5,23 @@
     (fn [rf]
       (fn
         ([] (rf))
-        ([res] (rf (rf res @buffer)))
+        ([res]
+         (if-let [buffer' (seq @buffer)]
+           (rf (rf res buffer'))
+           (rf res)))
         ([res item]
          (if-let [buffer' @buffer]
            (if (pred item)
              (do
                (vreset! buffer [item])
-               (rf res buffer'))
+               (if (seq buffer')
+                 (rf res buffer')
+                 res))
              (do
                (vswap! buffer conj item)
                res))
-           (do (vreset! buffer [item]) res)))))))
+           (do (vreset! buffer [item])
+               res)))))))
 
 (def take-until (comp take-while complement))
 (def drop-until (comp drop-while complement))
